d58c7b69ddd2f0a520a9a7dfb5f75b194392961e
Ashley Boswell <boswelas@oregonstate.edu>
2023-07-10 17:10:46 +0900

    set up flask app

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,10 @@
-from flask import Flask, jsonify, request, json
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import os
-import mysql.connector
-
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app)
-
-HOST = os.environ['MYSQLHOST']
-PORT = os.environ['MYSQLPORT']
-USER = os.environ['MYSQLUSER']
-PASSWORD = os.environ['MYSQLPASSWORD']
-DATABASE = os.environ['MYSQLDATABASE']
-GEOCAGE_API = os.environ['GEOCAGE_API']
-
-def create_connection():
-    db = mysql.connector.connect(
-        host=HOST,
-        port=PORT,
-        user=USER,
-        password=PASSWORD,
-        database=DATABASE
-    )
-
-    return db
-
 
 @app.route('/')
-def index():
-    print("Server is running!")
-    return jsonify("Welcome to the Travel Planner backend!")
-
+def hello():
+    return 'Hello, Backend!'
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5001))
+    app.run(debug=True)
