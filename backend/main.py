72fd1aacb057a221e83c09489827b1d21d99bff9
Ashley Boswell <boswelas@oregonstate.edu>
2023-07-15 17:22:09 +0900

    connected db in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
+HOST = os.environ['MYSQLHOST']
+PORT = os.environ['MYSQLPORT']
+USER = os.environ['MYSQLUSER']
+PASSWORD = os.environ['MYSQLPASSWORD']
+DATABASE = os.environ['MYSQLDATABASE']
+
 
 @app.route('/')
 def hello():
     return 'Hello, Backend!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5001))
