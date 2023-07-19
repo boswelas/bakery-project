129a95c224fcff2e7284881a47cd52383144cf06
Ashley Boswell <boswelas@oregonstate.edu>
2023-07-19 13:54:21 +0900

    corrected key error

--- frontend/src/components/InventoryGrid.js
+++ frontend/src/components/InventoryGrid.js
@@ -7,24 +7,23 @@ import InventoryCard from "./InventoryCard";
 
 
 function InventoryGrid({ data }) {
-    const inventoryItems = data.map((cardData, index) => (
-        <div key={index} >
+    const inventoryItems = data.map((cardData) => (
+        <div key={cardData.id}>
             <InventoryCard props={cardData} />
         </div>
     ));
 
     return (
-        <>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                {inventoryItems.map((item => (
-                    <Grid item xs={6}>
-                        {item}
-                    </Grid>
-                )))}
 
-            </Grid>
-        </>
-    )
-};
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+            {inventoryItems.map((item, index) => (
+                <Grid xs={6} key={index}>
+                    {item}
+                </Grid>
+            ))}
+        </Grid>
+
+    );
+}
 
 export default InventoryGrid;
